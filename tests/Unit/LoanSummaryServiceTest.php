a6092a55db2e6c836f72ed05c2bd53967c8700d0
qistina <naqistina00@gmail.com>
2025-09-05 18:06:30 +0800

    add test passed ss

--- tests/Unit/LoanSummaryServiceTest.php
+++ tests/Unit/LoanSummaryServiceTest.php
@@ -17,7 +17,7 @@ class LoanSummaryServiceTest extends TestCase
     public function it_calculates_summary_correctly()
     {
         $loan = Loan::factory()->create([
-            'principal_cents' => '1000000', // 10,000.00
+            'principal_cents' => '1000000', // RM10000.00
         ]);
 
         Repayment::factory()->create([
@@ -32,6 +32,8 @@ class LoanSummaryServiceTest extends TestCase
 
         $service = new LoanSummaryService();
         $summary = $service->getSummary($loan);
+
+//        $this->assertEquals($expected, $actual);
 
         $this->assertEquals("500000", $summary['total_paid_cents']);
         $this->assertEquals("500000", $summary['outstanding_principal_cents']);
